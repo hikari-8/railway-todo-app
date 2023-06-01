1556813620491656b2da8c810d9bf7e3d53de65b
hikari-8 <kanbarusuruga0127@gmail.com>
2023-06-01 21:47:09 +0900

    eslintのルールを変更

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   ],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn'
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off' // prop-typesのチェックを無効にする
   },
 };
